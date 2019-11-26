c33ffd8a111b7b54fec0d124338fbe901751acce
EricNeid <eric.neidhardt@gmail.com>
2019-11-26 09:08:39 +0100

    Print parsed output as json

--- cmd/netconvert/main.go
+++ cmd/netconvert/main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/EricNeid/go-netconvert"
 	"github.com/EricNeid/go-netconvert/internal/util"
+	"github.com/EricNeid/go-netconvert/osm"
+	"github.com/EricNeid/go-netconvert/writer"
 )
 
 func main() {
@@ -16,9 +18,9 @@ func main() {
 		return
 	}
 
-	defer util.TimeTrack(time.Now(), "Parsing")
 	xmlFile := arg[0]
-	net, err := netconvert.Decode(xmlFile)
+
+	net, err := parseFile(xmlFile)
 	if err != nil {
 		util.Error("main", err)
 		return
@@ -27,4 +29,14 @@ func main() {
 	fmt.Printf("Finished parsing xml\n")
 	fmt.Printf("Number of nodes: %d\n", len(net.Nodes))
 	fmt.Printf("Number of ways:  %d\n", len(net.Ways))
+
+	fmt.Printf("Writing output\n")
+	writer.NodesAsJSON(net.Nodes, "out-nodes.json")
+	writer.WaysAsJSON(net.Ways, "out-ways.json")
+}
+
+func parseFile(xmlFile string) (*osm.Net, error) {
+	fmt.Printf("Reading input\n")
+	defer util.TimeTrack(time.Now(), "Parsing")
+	return netconvert.Decode(xmlFile)
 }
